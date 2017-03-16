fa847ea95ba9966a5dc1cbf911385e6edd425bb4
khoi123 <khoi123@KhoiNTSE63078.local>
2017-03-17 00:38:12 +0700

    Inital commit

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
 const express = require('express');
 const hbs = require('hbs');
-const port = process.env.PORT || 3000 ;
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
- var now = new Date().toString();
-
- console.log(`${now}: ${req.method} , this user has ip${req.ip} , This user is in ${req.originalUrl}  `);
-  next()
-});
-
-app.use((req, res, next) => {
-  res.render('message.hbs')
-});
-
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
-  // res.send('<h1>hello express!</h1>');
-res.render('home.hbs',{
-  pageTitle: 'Home page',
-  wellcomeMessage: 'Welcom to my website'
-    })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
+});
 
-app.get('/about',(req, res) => {
+app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About page'
-  })
-});
-
-app.get('/help',(req, res) => {
-  res.render('help.hbs', {
-    pageTitle: 'Help page'
-  })
-});
-
-
-app.get('/bad', (req, res) => {
-    res.send({
-      errorMessage:'Unable handler connection'
-    }
-   )
+    pageTitle: 'About Page'
   });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
-  console.log(`server is up on port ${port} ` );
+  console.log(`Server is up on port ${port}`);
 });
